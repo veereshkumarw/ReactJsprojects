3f5beb710b6949235910209dad1a7bd94fece188
veereshkumarw <130819983+veereshkumarw@users.noreply.github.com>
2024-03-09 16:53:02 +0530

    Delete recipe directory

--- recipe/src/constants/Constant.js
+++
@@ -1,3 +0,0 @@
-export const logo = 'https://icon2.cleanpng.com/20180502/qxw/kisspng-indian-cuisine-organic-food-pav-bhaji-cooking-5aea2e434bbf68.8968052815252967073103.jpg';
-
-export const API_URL = 'https://forkify-api.herokuapp.com/api';
